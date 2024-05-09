40706fa0f66bd37921eeb635558b01b49d51ff4e
77Jokin <jokinmel@gmail.com>
2024-05-09 13:50:09 +0200

    v.4.2

---
+++ 14_Proyecto_para_evaluacion/src/modelo/entidades/Producto.java
@@ -0,0 +1,123 @@
+package modelo.entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+
+/**
+ * The persistent class for the productos database table.
+ * 
+ */
+@Entity
+@Table(name="productos")
+@NamedQuery(name="Producto.findAll", query="SELECT p FROM Producto p")
+public class Producto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_producto")
+	private int idProducto;
+
+	private String descripcion;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="fecha_creacion")
+	private Date fechaCreacion;
+
+	private BigDecimal precio;
+
+	private int stock;
+
+	//uni-directional many-to-one association to Familia
+	@ManyToOne
+	@JoinColumn(name="id_familia")
+	private Familia familia;
+
+	public Producto(int idProducto, String descripcion, Date fechaCreacion, BigDecimal precio, int stock,
+			Familia familia) {
+		super();
+		this.idProducto = idProducto;
+		this.descripcion = descripcion;
+		this.fechaCreacion = fechaCreacion;
+		this.precio = precio;
+		this.stock = stock;
+		this.familia = familia;
+	}
+
+	public Producto() {
+	}
+
+	public int getIdProducto() {
+		return this.idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFechaCreacion() {
+		return this.fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public BigDecimal getPrecio() {
+		return this.precio;
+	}
+
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return this.stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Familia getFamilia() {
+		return this.familia;
+	}
+
+	public void setFamilia(Familia familia) {
+		this.familia = familia;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idProducto);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Producto))
+			return false;
+		Producto other = (Producto) obj;
+		return idProducto == other.idProducto;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [idProducto=" + idProducto + ", descripcion=" + descripcion + ", fechaCreacion="
+				+ fechaCreacion + ", precio=" + precio + ", stock=" + stock + ", familia=" + familia + "]";
+	}
+
+}
---
+++ 14_Proyecto_para_evaluacion/src/modelo/entidades/ProyectoConProducto.java
@@ -0,0 +1,112 @@
+package modelo.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the proyecto_con_productos database table.
+ * 
+ */
+@Entity
+@Table(name="proyecto_con_productos")
+@NamedQuery(name="ProyectoConProducto.findAll", query="SELECT p FROM ProyectoConProducto p")
+public class ProyectoConProducto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="num_orden")
+	private int numOrden;
+
+	private int cantidad;
+
+	@Column(name="precio_asignado")
+	private int precioAsignado;
+
+	//uni-directional many-to-one association to Producto
+	@ManyToOne
+	@JoinColumn(name="id_producto")
+	private Producto producto;
+
+	//uni-directional many-to-one association to Proyecto
+	@ManyToOne
+	@JoinColumn(name="id_proyecto")
+	private Proyecto proyecto;
+
+	public ProyectoConProducto(int numOrden, int cantidad, int precioAsignado, Producto producto, Proyecto proyecto) {
+		super();
+		this.numOrden = numOrden;
+		this.cantidad = cantidad;
+		this.precioAsignado = precioAsignado;
+		this.producto = producto;
+		this.proyecto = proyecto;
+	}
+
+	public ProyectoConProducto() {
+	}
+
+	public int getNumOrden() {
+		return this.numOrden;
+	}
+
+	public void setNumOrden(int numOrden) {
+		this.numOrden = numOrden;
+	}
+
+	public int getCantidad() {
+		return this.cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public int getPrecioAsignado() {
+		return this.precioAsignado;
+	}
+
+	public void setPrecioAsignado(int precioAsignado) {
+		this.precioAsignado = precioAsignado;
+	}
+
+	public Producto getProducto() {
+		return this.producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Proyecto getProyecto() {
+		return this.proyecto;
+	}
+
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(numOrden);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof ProyectoConProducto))
+			return false;
+		ProyectoConProducto other = (ProyectoConProducto) obj;
+		return numOrden == other.numOrden;
+	}
+
+	@Override
+	public String toString() {
+		return "ProyectoConProducto [numOrden=" + numOrden + ", cantidad=" + cantidad + ", precioAsignado="
+				+ precioAsignado + ", producto=" + producto + ", proyecto=" + proyecto + "]";
+	}
+
+}
